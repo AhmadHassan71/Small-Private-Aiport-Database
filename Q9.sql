d68c1cef140470d269b0b1014a6989312bf1261b
Ahmad Hassan <96523778+AhmadHassan71@users.noreply.github.com>
2023-03-27 00:35:36 +0500

    Delete Q9.sql

--- Q9.sql
+++
@@ -1,6 +0,0 @@
---Q#9
-SELECT PERSON.P_Name,count(FLIES.OF_TYPE)
-FROM PILOT
-inner join PERSON on PILOT.Pi_Ssn = PERSON.P_Ssn
-inner join FLIES on PILOT.Lic_No = FLIES.Lic_num
-group by P_Name, Lic_No
