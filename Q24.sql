3366543c5731da3e6ab6cbe5b63980a6dc1cc4fd
Ahmad Hassan <96523778+AhmadHassan71@users.noreply.github.com>
2023-03-27 00:36:40 +0500

    Delete Q24.sql

--- Q24.sql
+++
@@ -1,6 +0,0 @@
---Q#24
-SELECT PILOT.Lic_No, COUNT(FLIES.OF_TYPE) as Num_Types_Planes_Auth
-from PILOT
-join FLIES ON flies.Lic_num=PILOT.Lic_No
-group by PILOT.Lic_No
-order by Num_Types_Planes_Auth desc;
