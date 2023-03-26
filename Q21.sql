44285c1b9c40dc848bc665c35224042f3adfcf35
Ahmad Hassan <96523778+AhmadHassan71@users.noreply.github.com>
2023-03-27 00:36:25 +0500

    Delete Q21.sql

--- Q21.sql
+++
@@ -1,6 +0,0 @@
---Q#21
-SELECT PLANE_SERVICE.Reg#, COUNT(*) as Total_Services
-from SERVICE_RECORD
-join PLANE_SERVICE ON PLANE_SERVICE.ServiceID=SERVICE_RECORD.Service_Id
-group by PLANE_SERVICE.Reg#
-order by total_services desc;
