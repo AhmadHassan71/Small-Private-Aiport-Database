5adc673ed69082202f74445c501545a556e08b23
Ahmad Hassan <96523778+AhmadHassan71@users.noreply.github.com>
2023-03-27 00:35:53 +0500

    Delete Q14.sql

--- Q14.sql
+++
@@ -1,8 +0,0 @@
---Q#14
-SELECT DISTINCT c.C_Name,c.C_Phone
-FROM OWNER o
-JOIN OWNS ow ON o.O_ID = ow.OwnerID
-JOIN CORPORATION c ON o.C_ID = c.C_ID
-join AIRPLANE a ON ow.Reg# = a.Reg#
-JOIN HANGAR h ON a.Stored_In = h.Number
-WHERE c.C_Address = h.H_Location;
