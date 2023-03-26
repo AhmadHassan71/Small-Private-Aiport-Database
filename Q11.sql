eb3051ba84b89e08f1d06b0583ee729395bc0916
Ahmad Hassan <96523778+AhmadHassan71@users.noreply.github.com>
2023-03-27 00:35:41 +0500

    Delete Q11.sql

--- Q11.sql
+++
@@ -1,8 +0,0 @@
---q#11
-
-select corporation.C_ID,CORPORATION.C_Name, count(OWNS.Reg#) as Num_Of_Planes
-from CORPORATION 
-inner join owner on CORPORATION.C_ID=OWNER.C_ID
-inner join OWNS on Owner.O_ID=OWNS.OwnerID
-group by Corporation.C_ID, CORPORATION.C_Name, Owns.Reg#
-order by Num_Of_Planes desc;
