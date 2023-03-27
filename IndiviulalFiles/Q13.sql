7acdac0bc979af7acd2c7896fd39331bd0ede89e
Ahmad Hassan <96523778+AhmadHassan71@users.noreply.github.com>
2023-03-27 07:48:51 +0500

    Update Q13.sql

--- IndiviulalFiles/Q13.sql
+++ IndiviulalFiles/Q13.sql
@@ -1,4 +1,3 @@
-
 --Q#13
 SELECT DISTINCT o.O_ID,PERSON.P_Name,CORPORATION.C_Name
 FROM Owner o
@@ -6,8 +5,9 @@ INNER JOIN owns ON owns.OwnerID = o.O_ID
 Inner JOIN PERSON on Person.P_Ssn=o.P_Ssn
 Inner JOIN CORPORATION on CORPORATION.C_ID=o.C_ID
 INNER JOIN AIRPLANE a ON a.Reg# = owns.Reg#
-JOIN Employee e ON e.E_ID = e.E_ID
-JOIN SERVICE_RECORD s ON s.Service_Id = e.E_ID
-JOIN PLANE_SERVICE p ON p.ServiceID = s.Service_Id
+JOIN PLANE_SERVICE p ON p.Reg# = a.Reg#
+JOIN SERVICE_RECORD s ON s.Service_Id = p.ServiceID
+JOIN WORKS_ON w ON w.Service_Id = s.Service_Id
+JOIN Employee e ON e.E_ID = w.EmpID
 WHERE s.Work_Code IS NULL
-group by o.O_ID,PERSON.P_Name,CORPORATION.C_Name
+group by o.O_ID,PERSON.P_Name,CORPORATION.C_Name;
